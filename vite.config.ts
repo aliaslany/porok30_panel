a1897a8bd80496b740a425eb0ae0ec8b05350bd7
aliaslani <a77aslani@gmail.com>
2026-07-24 18:30:48 +0330

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/porok30_panel/',
   plugins: [react(), tailwindcss()],
   server: {
     host: '0.0.0.0',
